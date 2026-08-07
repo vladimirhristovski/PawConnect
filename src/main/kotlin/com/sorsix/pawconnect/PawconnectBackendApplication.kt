4101ea7a4e403ca03ea3bbfeaa06676e4ce51736
vladimirhristovski <vladimir13hristovski@gmail.com>
2026-08-07 05:17:02 +0200

    Initial commit, setting folder structure, project initialization

---
+++ src/main/kotlin/com/sorsix/pawconnect/PawconnectBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sorsix.pawconnect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PawconnectBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PawconnectBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/sorsix/pawconnect/PawconnectBackendApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.sorsix.pawconnect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class PawconnectBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
