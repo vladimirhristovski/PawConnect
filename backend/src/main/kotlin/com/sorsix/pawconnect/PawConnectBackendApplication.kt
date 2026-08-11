a4fd3555bd73cc84eb55ad0c1747634e115a696c
vladimirhristovski <vladimir13hristovski@gmail.com>
2026-08-11 03:02:25 +0200

    small changes

--- backend/src/main/kotlin/com/sorsix/pawconnect/PawConnectBackendApplication.kt
+++ backend/src/main/kotlin/com/sorsix/pawconnect/PawConnectBackendApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class PawconnectBackendApplication
+class PawConnectBackendApplication
 
 fun main(args: Array<String>) {
-    runApplication<PawconnectBackendApplication>(*args)
+    runApplication<PawConnectBackendApplication>(*args)
 }
