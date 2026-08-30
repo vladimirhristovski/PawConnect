a2e60cbfbbb84c7ce39bcc0d4e1fb8a7e291af5a
arsandrej <andrej.arsovski.1@students.finki.ukim.mk>
2026-08-30 19:36:15 +0200

    add geo search filter for listings

--- frontend/src/app/features/listings/listing-search/listing-search.ts
+++ frontend/src/app/features/listings/listing-search/listing-search.ts
@@ -1,10 +1,16 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
+import { Component, inject, signal, OnInit, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { ListingService } from '../../../core/services/listing';
 import { LookupService } from '../../../core/services/lookup';
 import { Pagination } from '../../../shared/pagination/pagination';
 import { ListingSearchParams } from '../../../core/models/listing.model';
+import { getCurrentPosition } from '../../../shared/geo/geo-utils';
+
+const FILTER_DEBOUNCE_MS = 400;
 
 @Component({
   selector: 'app-listing-search',
@@ -15,10 +21,20 @@ import { ListingSearchParams } from '../../../core/models/listing.model';
 export class ListingSearch implements OnInit {
   protected listingService = inject(ListingService);
   protected lookup = inject(LookupService);
+  private destroyRef = inject(DestroyRef);
 
   filters: ListingSearchParams = { page: 0, size: 12 };
   useNearby = signal(false);
+  locating = signal(false);
   locationError = signal<string | null>(null);
+
+  private filterChange$ = new Subject<void>();
+
+  constructor() {
+    this.filterChange$
+      .pipe(debounceTime(FILTER_DEBOUNCE_MS), takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => this.runSearch());
+  }
 
   ngOnInit(): void {
     this.lookup.loadSpecies();
@@ -30,6 +46,19 @@ export class ListingSearch implements OnInit {
     this.filters.page = 0;
     this.runSearch();
   }
+
+  onFilterChange(): void {
+    this.filters.page = 0;
+    this.filterChange$.next();
+  }
+
+  clearFilters(): void {
+    this.filters = { page: 0, size: 12 };
+    this.useNearby.set(false);
+    this.locationError.set(null);
+    this.search();
+  }
+
   goToPage(page: number): void {
     this.filters.page = page;
     this.runSearch();
@@ -49,22 +78,19 @@ export class ListingSearch implements OnInit {
   }
 
   private locateThenSearch(): void {
-    if (!navigator.geolocation) {
-      this.locationError.set('Geolocation is not supported by this browser.');
-      this.useNearby.set(false);
-      return;
-    }
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        this.filters.lat = pos.coords.latitude;
-        this.filters.lng = pos.coords.longitude;
+    this.locating.set(true);
+    getCurrentPosition()
+      .then((coords) => {
+        this.locating.set(false);
+        this.filters.lat = coords.lat;
+        this.filters.lng = coords.lng;
         this.search();
-      },
-      () => {
-        this.locationError.set('Could not get your location.');
+      })
+      .catch((err: Error) => {
+        this.locating.set(false);
+        this.locationError.set(err.message);
         this.useNearby.set(false);
-      },
-    );
+      });
   }
 
   private runSearch(): void {
